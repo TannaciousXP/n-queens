cf97740592db256a2a9160e26661d67aac15bd17
Hack Reactor Student <student@hackreactor.com>
2017-05-24 20:05:27 -0700

    Significant progress on nRooks

--- src/solvers.js
+++ src/solvers.js
@@ -16,10 +16,54 @@
 
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+  // create count var
+  // if count = n
+  // return chess;
+  // var count = 0;
+  // var matrix = [];
+  // for (var i = 0; i < n; i++) {
+  // console.log(n);
+  var numBoard = n;
+  var solution = new Board({n: numBoard});
+  var solutionsArr = [];
+  var count = 0;
+  var idx = 0;
+  var row = solution.rows()[idx];
+  var length = solution.rows().length;
 
-  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  return solution;
+  var findSolution = function(curRow) {
+    //toggle first value [1,0,0]
+    for (var i = 0; i < row.length; i++) {
+      solution.togglePiece(idx, i);
+      // console.dir(solution);
+      if (!solution.hasAnyRowConflicts() && !solution.hasAnyColConflicts()) {
+        count++;
+        idx++;
+        if (count === n) {
+          return solution;
+        }
+        return findSolution(row);
+      } else {
+        solution.togglePiece(idx, i);
+      }
+      
+    }
+    //toggle next value
+    //checks if conflicts exist
+      //if exists toggle back
+    //else 
+      //count ++ 
+      //check if count is equal to n 
+      //move to next row
+
+    //loop through solution height
+      //grab row and throw into findSolution 
+  
+  };
+
+  var output = findSolution(solution);
+  console.log(output);
+  return output.rows();
 };
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
